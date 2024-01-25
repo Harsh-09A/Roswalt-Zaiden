20b61afff01459760b0b683e7c0fc85873fc8e24
Harsh Athavale <harsh.autowebbed@gmail.com>
2024-01-25 17:43:12 +0530

    Made Privacy Changes

--- js/script.js
+++ js/script.js
@@ -28,3 +28,20 @@ $(window).ready(function () {
     $("#popupModal").modal("show");
   }, 3000);
 });
+
+// Toggle Content Button
+function toggleContent(dotsId, moreTextId, btnId) {
+  var dots = document.getElementById(dotsId);
+  var moreText = document.getElementById(moreTextId);
+  var btnText = document.getElementById(btnId);
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read More ...";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read Less";
+    moreText.style.display = "inline";
+  }
+}
